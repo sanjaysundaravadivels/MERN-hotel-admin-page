5f733e202be1b39701cbb4e103a9af477e4991b1
sanjay s <sanman014@gmail.com>
2021-08-05 20:46:41 +0530

    commit

--- server.js
+++ server.js
@@ -20,5 +20,15 @@ app.use("/api/feedback", require("./routes/api/feedback"));
 app.use("/api/orderstatus", require("./routes/api/orderStatus"));
 app.use("/api/leave", require("./routes/api/leave"));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
